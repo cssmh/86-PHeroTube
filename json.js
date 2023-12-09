e6cdf8f87cb8ba6c2cb74a14a58aa38a0ee938f8
momen210 <touristmomen@gmail.com>
2023-12-09 12:56:01 +0600

    header completed

--- json.js
+++ json.js
@@ -1,1 +1,6 @@
-
+const handleTube = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await res.json();
+    console.log(data.data);
+}
+handleTube()
